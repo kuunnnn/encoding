43a695d20e245d28a1da23a2f67da2010c97e222
胡坤 <mrhukun@gmail.com>
2020-06-24 11:43:27 +0800

    新增测试

--- test/base.test.js
+++ test/base.test.js
@@ -1,6 +1,32 @@
-const { toEight, toBinary, toHex } = require("../lib/base");
+const {
+  toEight,
+  toBinary,
+  toHex,
+  IEEE754Float32_decode,
+  float64ToBinary,
+  IEEE754Float32_encode,
+} = require("../lib/base");
 
-describe("encode-base64", () => {
+describe("进制转换", () => {
+  test("IEEE 754 float32", () => {
+    expect(IEEE754Float32_encode(1.25)).toEqual(
+      "00111111101000000000000000000000"
+    );
+    expect(IEEE754Float32_decode("00111111101000000000000000000000")).toEqual(
+      1.25
+    );
+    expect(IEEE754Float32_encode(1.8)).toEqual(
+      "00111111111001100110011001100110"
+    );
+    expect(IEEE754Float32_decode("00111111111001100110011001100110")).toEqual(
+      1.7999999523162842
+    );
+  });
+
+  test("float 64 二进制", () => {
+    expect(float64ToBinary(1.345)).toEqual((1.345).toString("2"));
+  });
+
   test("to-hex", () => {
     expect(toHex(0xf)).toEqual("0xF");
     expect(toHex(0x56565)).toEqual("0x56565");
